37623861c3ed83665837fea02355ae8b32a86336
itosho <www.itosho@gmail.com>
2020-12-06 23:19:34 +0900

    Add a sample controller

---
+++ cakephp-sample/src/Controller/MessagesController.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Messages Controller
+ */
+class MessagesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        echo 'hey!';
+        exit;
+    }
+}
